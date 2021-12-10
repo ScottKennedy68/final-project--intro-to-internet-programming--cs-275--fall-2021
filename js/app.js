0409ea238990ac9e4cc84dbb20e0403d046457f0
ScottKennedy68 <skennedy@hartford.edu>
2021-12-09 21:38:28 -0500

    Added the left arrow's function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 let slide = document.querySelector(`.slide`);
+let leftArrow = document.getElementById(`left-arrow`);
 let rightArrow = document.getElementById(`right-arrow`);
-let currentSlideIndex = 1;
+let currentSlideIndex = 0;
+let slideWidth = 210;
+
+leftArrow.addEventListener(`click`, ()=>{
+    currentSlideIndex--;
+    slide.style.transform = `translateX(${-slideWidth * currentSlideIndex}px)`;
+});
 
 rightArrow.addEventListener(`click`, ()=>{
     currentSlideIndex++;
-    slide.style.transform = `translateX(${-210 * currentSlideIndex}px)`;
+    slide.style.transform = `translateX(${-slideWidth * currentSlideIndex}px)`;
 });
