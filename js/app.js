3e61c0fa6ee63c5eb9f5b7e4949249232ff1cf57
ScottKennedy68 <skennedy@hartford.edu>
2021-12-10 23:16:45 -0500

    Added keyboard listener

--- js/app.js
+++ js/app.js
@@ -14,5 +14,14 @@ function goToNextSlide() {
     slideShow.style.transform = `translateX(${-slideWidth * currentSlideIndex}px)`;
 }
 
+
 leftArrow.addEventListener(`click`, goToPreviousSlide, false);
 rightArrow.addEventListener(`click`, goToNextSlide, false);
+document.addEventListener(`keydown`, (key)=>{
+    //Key codes found on
+    //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+    if(key.keyCode === 37)  //Checks for left arrow key
+        goToPreviousSlide();
+    else if(key.keyCode === 39) //Checks for right arrow key
+        goToNextSlide();
+});
